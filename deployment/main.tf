58188bec0af233954857ad3b67405f25c02ec402
Annonator <andreaspohl89@outlook.com>
2022-12-23 11:24:10 +0100

    AppInsight and Function Deployment

--- deployment/main.tf
+++ deployment/main.tf
@@ -12,13 +12,41 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "analytics_ingest_rg" {
-  name     = "rg-prod-analytics-ingest"
+# Set up application insights
+resource "azurerm_resource_group" "appinsight_rg" {
+  name     = "rg-appinsight-${var.environment}"
   location = "West Europe"
 }
 
+resource "azurerm_application_insights" "appinsight" {
+  name                = "ai-insight-${var.environment}"
+  location            = azurerm_resource_group.appinsight_rg.location
+  resource_group_name = azurerm_resource_group.appinsight_rg.name
+  application_type    = "web"
+}
+
+output "instrumentation_key" {
+  value = azurerm_application_insights.appinsight.instrumentation_key
+  sensitive = true
+}
+
+output "appinsight_connectionString"{
+  value = azurerm_application_insights.appinsight.connection_string
+  sensitive = true
+}
+
+output "app_id" {
+  value = azurerm_application_insights.appinsight.app_id
+}
+
+# Setup Function
+resource "azurerm_resource_group" "analytics_ingest_rg" {
+  name     = "rg-analytics-ingest-${var.environment}"
+  location = var.location
+}
+
 resource "azurerm_storage_account" "analytics_ingest_storage" {
-  name                     = "linuxfunctionappsa"
+  name                     = "st${var.environment}analyticssa"
   resource_group_name      = azurerm_resource_group.analytics_ingest_rg.name
   location                 = azurerm_resource_group.analytics_ingest_rg.location
   account_tier             = "Standard"
@@ -26,7 +54,7 @@ resource "azurerm_storage_account" "analytics_ingest_storage" {
 }
 
 resource "azurerm_service_plan" "analytics_ingest_functionplan" {
-  name                = "example-app-service-plan"
+  name                = "func-analyticsplan-${var.environment}"
   resource_group_name = azurerm_resource_group.analytics_ingest_rg.name
   location            = azurerm_resource_group.analytics_ingest_rg.location
   os_type             = "Linux"
@@ -34,7 +62,7 @@ resource "azurerm_service_plan" "analytics_ingest_functionplan" {
 }
 
 resource "azurerm_linux_function_app" "analytics_ingest_functionapp" {
-  name                = "example-linux-function-app"
+  name                = "func-analyticsIngest-${var.environment}"
   resource_group_name = azurerm_resource_group.analytics_ingest_rg.name
   location            = azurerm_resource_group.analytics_ingest_rg.location
 
@@ -42,5 +70,12 @@ resource "azurerm_linux_function_app" "analytics_ingest_functionapp" {
   storage_account_access_key = azurerm_storage_account.analytics_ingest_storage.primary_access_key
   service_plan_id            = azurerm_service_plan.analytics_ingest_functionplan.id
 
-  site_config {}
+  site_config {
+    application_insights_connection_string = azurerm_application_insights.appinsight.connection_string
+    application_insights_key = azurerm_application_insights.appinsight.instrumentation_key
+    application_stack {
+      dotnet_version = "7.0"
+      use_dotnet_isolated_runtime = true
+    }
+  }
 }
---
+++ deployment/variables.tf
@@ -0,0 +1,9 @@
+﻿variable "location" {
+  type = string
+  description = "Azure region to deploy module to"
+}
+
+variable "environment" {
+  type = string
+  description = "Environment (dev / stage / prod)"
+}
